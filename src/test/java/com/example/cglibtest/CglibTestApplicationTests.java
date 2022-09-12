46d2643178ce9d42e46637e58d476400c4254eef
martin <martin@niceday.io>
2022-09-12 18:41:27 +0900

    first

---
+++ src/test/java/com/example/cglibtest/CglibTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cglibtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CglibTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
